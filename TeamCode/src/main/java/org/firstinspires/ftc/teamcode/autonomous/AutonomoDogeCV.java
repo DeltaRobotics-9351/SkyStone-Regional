38932017dd0d58f241c551c857c8dafbe46a1ef2
d4n1_63 <serivesmejia@gmail.com>
2019-10-18 22:22:21 -0600

    Implementado DogeCV y agregado un autonomo de prueba

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomoDogeCV.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+
+import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+import java.util.Locale;
+
+@Autonomous(name="Autonomo DogeCV Test", group="DogeCV")
+
+public class AutonomoDogeCV extends LinearOpMode {
+    private OpenCvCamera phoneCam;
+    private SkystoneDetector skyStoneDetector;
+
+    @Override
+    public void runOpMode() {
+        //creamos la vista desde el celular de la camara
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        //creamos la camara de OpenCV
+        phoneCam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+
+
+        //abrimos la camara trasera del dipositivo
+        phoneCam.openCameraDevice();
+
+        //creamos el detector de skystones
+        skyStoneDetector = new SkystoneDetector();
+        phoneCam.setPipeline(skyStoneDetector); //definimos que la pipeline sera el skystonedetector
+                                                //es decir, los frames iran hacia este.
+
+        //empezamos a trasmitir/stremear la camara del celular trasera
+        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+
+        //esperamos que el usuario presione <play> en la driver station
+        waitForStart();
+
+        while (opModeIsActive()) {
+            //enviamos mensajes telemetry que contienen informacion
+            telemetry.addData("Stone Position X", skyStoneDetector.getScreenPosition().x);
+            telemetry.addData("Stone Position Y", skyStoneDetector.getScreenPosition().y);
+            telemetry.addData("Frame Count", phoneCam.getFrameCount());
+            telemetry.addData("FPS", String.format(Locale.US, "%.2f", phoneCam.getFps()));
+            telemetry.addData("Total frame time ms", phoneCam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", phoneCam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", phoneCam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", phoneCam.getCurrentPipelineMaxFps());
+            telemetry.update();
+        }
+    }
+}
